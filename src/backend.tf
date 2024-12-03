8ca66806ffab814704ad014a711385133bb980e9
Rafael Vinicius <rafael.sampaio@loft.com.br>
2024-12-03 08:57:13 -0300

    fix: role and s3

--- src/backend.tf
+++ src/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "tech-challenge-fase-4-terraform"
+    bucket = "tech-challenge-fase-4-infra"
     key    = "eks/terraform.tfstate"
     region = "us-east-1"
   }
--- src/local.tf
+++ src/local.tf
@@ -1,7 +1,7 @@
 locals {
   aws_region        = "us-east-1"
-  aws_role          = "arn:aws:iam::249752625560:role/LabRole"
-  aws_principal_arn = "arn:aws:iam::249752625560:role/voclabs"
+  aws_role          = "arn:aws:iam::211125701461:role/LabRole"
+  aws_principal_arn = "arn:aws:iam::211125701461:role/voclabs"
   aws_access_config = "API_AND_CONFIG_MAP"
   aws_policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
 
